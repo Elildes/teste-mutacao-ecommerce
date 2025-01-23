662dbbd618e8174727d1bd9a8c08280ede80cbaf
manueljonas <manuel.jonas@gmail.com>
2025-01-23 03:38:11 -0300

    Update da Classe CompraServiceTest

--- src/test/java/ecommerce/CompraServiceTest.java
+++ src/test/java/ecommerce/CompraServiceTest.java
@@ -17,7 +17,6 @@ import ecommerce.entity.TipoProduto;
 import ecommerce.service.CompraService;
 import java.math.RoundingMode;
 
-
 class CompraServiceTest {
 
     private CompraService compraService;
@@ -38,7 +37,7 @@ class CompraServiceTest {
     }
 
     @Test
-    void calcularCustoTotal_FreteLeve_DeveCalcularFreteCorretamente() {
+    void calcularCustoTotal_FreteLeve_DeveCalcularFrete() {
         Produto produto = new Produto(1L, "Produto A", "Descrição A", BigDecimal.valueOf(100.0), 2, TipoProduto.ELETRONICO); // 2 kg por unidade
         ItemCompra item = new ItemCompra(1L, produto, 2L); // 4 kg total
 
@@ -49,12 +48,12 @@ class CompraServiceTest {
 
         BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho);
 
-     // Usando setScale com RoundingMode
+        // Usando setScale com RoundingMode
         assertEquals(BigDecimal.valueOf(200.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 200 (produtos) + 0 (até 5 kg não é cobrado frete)
     }
 
     @Test
-    void calcularCustoTotal_FretePesado_DeveCalcularFreteCorretamente() {
+    void calcularCustoTotal_FretePesado_DeveCalcularFrete() {
         Produto produto = new Produto(1L, "Produto B", "Descrição B", BigDecimal.valueOf(150.0), 20, TipoProduto.MOVEL); // 20 kg por unidade
         ItemCompra item = new ItemCompra(1L, produto, 3L); // 60 kg total
 
@@ -65,13 +64,12 @@ class CompraServiceTest {
 
         BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho);
 
-     // Usando setScale com RoundingMode
+        // Usando setScale com RoundingMode
         assertEquals(BigDecimal.valueOf(870.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 450 (produtos) + 420 (acima de 50 kg é cobrado R$ 7,00 por kg)
     }
 
-
     @Test
-    void calcularCustoTotal_ComDescontoPorValor_DeveAplicarDescontoCorretamente() {
+    void calcularCustoTotal_ComDescontoPorValor_DeveAplicarDesconto() {
         Produto produto = new Produto(1L, "Produto C", "Descrição C", BigDecimal.valueOf(600.0), 1, TipoProduto.ROUPA); // 1 kg por unidade
         ItemCompra item = new ItemCompra(1L, produto, 1L); // 600 total
 
@@ -86,7 +84,6 @@ class CompraServiceTest {
         assertEquals(BigDecimal.valueOf(540.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 600 - 60 (desconto) + 0 (até 5 kg não é cobrado frete)
     }
 
-
     @Test
     void calcularCustoTotal_ClienteOuro_DeveAplicarDescontoTotalNoFrete() {
         Produto produto = new Produto(1L, "Produto D", "Descrição D", BigDecimal.valueOf(300.0), 15, TipoProduto.ELETRONICO); // 15 kg por unidade
@@ -99,7 +96,7 @@ class CompraServiceTest {
 
         BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho);
 
-     // Usando setScale com RoundingMode
+        // Usando setScale com RoundingMode
         assertEquals(BigDecimal.valueOf(300.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 300 + 0 (frete gratuito para OURO)
     }
 
@@ -115,7 +112,7 @@ class CompraServiceTest {
 
         BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho);
 
-     // Usando setScale com RoundingMode
+        // Usando setScale com RoundingMode
         assertEquals(BigDecimal.valueOf(330.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 300 + 30 (50% do frete)
     }
 
@@ -131,7 +128,25 @@ class CompraServiceTest {
 
         BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho);
 
-     // Usando setScale com RoundingMode
+        // Usando setScale com RoundingMode
         assertEquals(BigDecimal.valueOf(360.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 300 + 60 (frete completo)
+    }
+
+    // Teste adicional para verificação de desconto de 20%
+    @Test
+    void calcularCustoTotal_ComDescontoDe20Porcento_DeveAplicarDesconto() {
+        Produto produto = new Produto(1L, "Produto G", "Descrição G", BigDecimal.valueOf(1200.0), 2, TipoProduto.ROUPA); // 1200 total
+
+        ItemCompra item = new ItemCompra(1L, produto, 1L); // 1200 total
+
+        Cliente cliente = new Cliente(1L, "Marcos", "Rua H, 234", TipoCliente.BRONZE);
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.setCliente(cliente);
+        carrinho.setItens(List.of(item));
+
+        BigDecimal custoTotal = compraService.calcularCustoTotal(carrinho);
+
+        // Usando setScale com RoundingMode
+        assertEquals(BigDecimal.valueOf(960.0).setScale(2, RoundingMode.HALF_UP), custoTotal.setScale(2, RoundingMode.HALF_UP)); // 1200 - 240 (desconto de 20%)
     }
 }
